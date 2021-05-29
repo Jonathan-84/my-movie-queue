d55af7de82d08512717bfeefd5a8de153f501489
JRob <72467031+Jonathan-84@users.noreply.github.com>
2021-05-28 23:01:17 -0400

    errors with master

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -7,7 +7,7 @@ import Auth from '../utils/auth';
 function Nav(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container">
+        <div className="container-fluid">
             <a className="navbar-brand" href="/"><img className="logo" src={logo} alt="Kesiah's Logo"/></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon background-orange"></span>
