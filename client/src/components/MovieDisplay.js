2ce450606e696dbdd0e690ecf2c60bfdc4897786
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2023-03-14 10:17:42 -0400

    adding the json changes

--- client/src/components/MovieDisplay.js
+++ client/src/components/MovieDisplay.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, Component } from 'react';
 import Auth from '../utils/auth';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+// import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
-import { saveMovieIds, getSavedMovieIds } from '../utils/localStorage';
+// import { saveMovieIds, getSavedMovieIds } from '../utils/localStorage';
 
-// integrate Apollo Hooks
-import { SAVE_MOVIE } from '../utils/mutations';
-import {useMutation} from '@apollo/react-hooks';
+// // integrate Apollo Hooks
+// import { SAVE_MOVIE } from '../utils/mutations';
+// import {useMutation} from '@apollo/react-hooks';
 
 class MovieDisplay extends Component {
 
